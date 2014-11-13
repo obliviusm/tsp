d9b3dea7b53b24c5df84ed821f595906e49f081b
obliviusm <obliviusm@gmail.com>
2014-11-13 21:26:41 +0000

    fix distance func. it must return home

--- algorithms/local_search.rb
+++ algorithms/local_search.rb
@@ -21,14 +21,13 @@ class LocalSearch
     @x
   end
   
-  private
+  protected
   
   def neighborhood
     for i in 0...(@x.size - 1)
       for j in (i + 1)..(@x.size - 1)
-        next if i == j
         x = swap(i, j, @x)
-        #f = swap_distance(i, j, @x, @f)
+        #f = swap_distance2(i, j, @x.dup, @f)
         f = distance x
         yield x, f
       end
@@ -36,11 +35,25 @@ class LocalSearch
   end
   
   def swap i, j, x
-    x_copy = x
+    x_copy = x.dup
     x_copy[i], x_copy[j] = x_copy[j], x_copy[i]
     x_copy
   end
   
+  def distance x
+    length = 0
+    for i in 0...(x.size - 1)
+      #p [from, to, @w[from][to]]
+      length += w i, i+1, x
+    end
+    length += w x.size-1, 0, x
+    length
+  end
+  
+  def w i, j, x
+    @w[x[i]][x[j]]
+  end
+=begin
   def swap_distance i, j, x, f
     c = Array.new(8, 0) # [0, 0, 0, 0, 0, 0, 0, 0]
     positions = [[i-1, i], [i, i+1], [j-1, j], [j, j+1], [i-1, j], [j, i+1], [j-1,i], [i, j+1]]
@@ -50,21 +63,26 @@ class LocalSearch
         source_i, dest_i = x[s], x[d]
         p [s, d, source_i, dest_i]
         c[index] = @w[source_i][dest_i]
+        c[index] *= -1 if index < 4
       end
     end
     
     p c, positions, f
  
-    f - c[0] - c[1] - c[2] - c[3] + c[4] + c[5] + c[6] + c[7]
+    f - c.inject(:+)
   end
   
-  def distance x
-    length = 0
-    for i in 0...(x.size - 1)
-      from, to = x[i], x[i+1]
-      #p [from, to, @w[from][to]]
-      length += @w[from][to]
+  def swap_distance2 i, j, x, f
+    for k in 0...(x.size - 1)
+      next if ([k, k+1] & [i, j]).empty?
+      f -= @w[x[k]][x[k+1]]
     end
-    length
+    x = swap i, j, x
+    for k in 0...(x.size - 1)
+      next if ([k, k+1] & [i, j]).empty?
+      f += @w[x[k]][x[k+1]]
+    end
+    f
   end
+=end
 end
--- main.rb
+++ main.rb
@@ -2,11 +2,22 @@ require_relative "services/xml_parser"
 require_relative "algorithms/local_search"
 
 
-tcp_problem = get_tcp_problem "data/br17.xml"
-graph = tcp_problem[:graph]
-initial_solution = tcp_problem[:initialSolutions][:path][0][:vertex]
-#p initial_solution
-local_search = LocalSearch.new(graph, initial_solution)
-p local_search.x, local_search.f
-local_search.simple_local_search
-p local_search.x, local_search.f
+problems = ["br17"]
+table = {}
+problems.each do |filename| 
+  table[filename] = {}
+  tcp_problem = get_tcp_problem "data/#{filename}.xml"
+  graph = tcp_problem[:graph]
+  table[filename][:solutions] = []
+  tcp_problem[:initialSolutions][:path].each_with_index do |solution, i|
+    initial_solution = solution[:vertex]
+    table[filename][:solutions][i] = []
+    local_search = LocalSearch.new(graph, initial_solution)
+    table[filename][:solutions][i][0] = local_search.f
+    #p local_search.x, local_search.f
+    local_search.simple_local_search
+    table[filename][:solutions][i][1] = local_search.f
+    #p local_search.x, local_search.f
+  end
+end
+p table
