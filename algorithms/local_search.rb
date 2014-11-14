a994dd7afea0bb66206a9a003848fb8511053609
obliviusm <obliviusm@gmail.com>
2014-11-14 10:34:26 +0000

    start TSPSolution. but it's long refactoring so will do it later. add swap indices to shaffle them a little

--- algorithms/local_search.rb
+++ algorithms/local_search.rb
@@ -25,12 +25,26 @@ class LocalSearch
   protected
   
   def neighborhood
+    swap_indices.each do |i, j|
+      x = swap(i, j, @x)
+      f = swap_distance(i, j, @x.dup, @f)
+      #f = distance x
+      yield x, f
+    end
+  end
+  
+  def swap_indices
+    unless @swap_indices
+      set_swap_indices
+    end
+    @swap_indices
+  end
+  
+  def set_swap_indices
+    @swap_indices = []
     for i in 0...@n
       for j in (i + 1)..@n
-        x = swap(i, j, @x)
-        f = swap_distance(i, j, @x.dup, @f)
-        #f = distance x
-        yield x, f
+        @swap_indices.push [i, j]
       end
     end
   end
@@ -82,36 +96,4 @@ class LocalSearch
 =end
     f_new
   end
-=begin
-  def swap_distance i, j, x, f
-    c = Array.new(8, 0) # [0, 0, 0, 0, 0, 0, 0, 0]
-    positions = [[i-1, i], [i, i+1], [j-1, j], [j, j+1], [i-1, j], [j, i+1], [j-1,i], [i, j+1]]
-    positions.each_with_index do |(s, d), index| # source, dest, index
-      max_position = @x.size - 1
-      if s >= 0 && d >= 0 && s <= max_position && d <= max_position
-        source_i, dest_i = x[s], x[d]
-        p [s, d, source_i, dest_i]
-        c[index] = @w[source_i][dest_i]
-        c[index] *= -1 if index < 4
-      end
-    end
-    
-    p c, positions, f
- 
-    f + c.inject(:+)
-  end
-  
-  def swap_distance2 i, j, x, f
-    for k in 0...(x.size - 1)
-      next if ([k, k+1] & [i, j]).empty?
-      f -= @w[x[k]][x[k+1]]
-    end
-    x = swap i, j, x
-    for k in 0...(x.size - 1)
-      next if ([k, k+1] & [i, j]).empty?
-      f += @w[x[k]][x[k+1]]
-    end
-    f
-  end
-=end
 end
